4d0063106ef9529fc91d1b1161ba7146fd74b159
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-19 16:27:04 -0500

    baller!

--- api/schemes/scheme-router.js
+++ api/schemes/scheme-router.js
@@ -6,29 +6,29 @@ const Schemes = require('./scheme-model.js')
 const router = express.Router()
 
 /**
-  [GET] /api/schemes
+ [GET] /api/schemes
 
-  response:
-  [
-    {
+ response:
+ [
+ {
       "scheme_id": 1,
       "scheme_name": "World Domination",
       "number_of_steps": 3
     },
-    {
+ {
       "scheme_id": 2,
       "scheme_name": "Get Rich Quick",
       "number_of_steps": 2
     },
-    // etc
-  ]
+ // etc
+ ]
  */
 router.get('/', (req, res, next) => {
-  Schemes.find()
-    .then(schemes => {
-      res.json(schemes)
-    })
-    .catch(next)
+    Schemes.find()
+        .then(schemes => {
+            res.json(schemes)
+        })
+        .catch(next)
 })
 
 /*
@@ -53,13 +53,13 @@ router.get('/', (req, res, next) => {
   }
 */
 router.get('/:scheme_id', checkSchemeId, (req, res, next) => {
-  const { scheme_id } = req.params
+    const {scheme_id} = req.params
 
-  Schemes.findById(scheme_id)
-    .then(scheme => {
-      res.json(scheme)
-    })
-    .catch(next)
+    Schemes.findById(scheme_id)
+        .then(scheme => {
+            res.json(scheme)
+        })
+        .catch(next)
 })
 
 /*
@@ -82,13 +82,13 @@ router.get('/:scheme_id', checkSchemeId, (req, res, next) => {
   ]
 */
 router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
-  const { scheme_id } = req.params
+    const {scheme_id} = req.params
 
-  Schemes.findSteps(scheme_id)
-    .then(steps => {
-      res.json(steps)
-    })
-    .catch(next)
+    Schemes.findSteps(scheme_id)
+        .then(steps => {
+            res.json(steps)
+        })
+        .catch(next)
 })
 
 /*
@@ -101,13 +101,13 @@ router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
   }
 */
 router.post('/', validateScheme, (req, res, next) => {
-  const scheme = req.body
+    const scheme = req.body
 
-  Schemes.add(scheme)
-    .then(scheme => {
-      res.status(201).json(scheme)
-    })
-    .catch(next)
+    Schemes.add(scheme)
+        .then(scheme => {
+            res.status(201).json(scheme)
+        })
+        .catch(next)
 })
 
 /*
@@ -130,22 +130,22 @@ router.post('/', validateScheme, (req, res, next) => {
   ]
 */
 router.post('/:scheme_id/steps', checkSchemeId, validateStep, (req, res, next) => {
-  const step = req.body
-  const { scheme_id } = req.params
+    const step = req.body
+    const {scheme_id} = req.params
 
-  Schemes.addStep(scheme_id, step)
-    .then(allSteps => {
-      res.status(201).json(allSteps)
-    })
-    .catch(next)
+    Schemes.addStep(scheme_id, step)
+        .then(allSteps => {
+            res.status(201).json(allSteps)
+        })
+        .catch(next)
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(err.status || 500).json({
-    sageAdvice: 'Finding the real error is 90% of the bug fix',
-    message: err.message,
-    stack: err.stack,
-  })
+    res.status(err.status || 500).json({
+        sageAdvice: 'Finding the real error is 90% of the bug fix',
+        message: err.message,
+        stack: err.stack,
+    })
 })
 
 module.exports = router
